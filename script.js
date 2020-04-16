1634825e9f866493c87b1e05140dde6b0ce58dcc
HeavenlyRaccoon <vova55565@gmail.com>
2020-04-17 02:19:06 +0300

    Сверстаны все страницы с меню и формой заказов, сделана система ссылок

--- script.js
+++ script.js
@@ -33,21 +33,48 @@ $(".whatchCatalog").hover(function () {
 });
 $("a.scrollto").click(function () {
     let elementClick = $(this).attr("href")
-    let destination = $(elementClick).offset().top-110;
-    $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+    let destination = $(elementClick).offset().top - 110;
+    $("html:not(:animated),body:not(:animated)").animate({
+        scrollTop: destination
+    }, 1100);
     return false;
-    });
-var check=true;
-$("#menuOpen").click(function(){
+});
+var check = true;
+$("#menuOpen").click(function () {
     console.log("LLL");
     $(".bottomHeader, .containerHeadButton, header h1").slideToggle();
     $(".menu").slideToggle();
-    if(check){
-        $(".burgerMenuDeckstop img").attr("src","/floristry/pictures/closeMenuDeck.svg");
-        check=!check;
-    }
-    else{
-        $(".burgerMenuDeckstop img").attr("src","/floristry/pictures/burgerMenuDeckstop.svg");
-        check=!check;
+    if (check) {
+        $(".burgerMenuDeckstop img").attr("src", "/floristry/pictures/closeMenuDeck.svg");
+        check = !check;
+    } else {
+        $(".burgerMenuDeckstop img").attr("src", "/floristry/pictures/burgerMenuDeckstop.svg");
+        check = !check;
     }
 });
+
+function order() {
+    $(".darkwindow").css("display", "block");
+    $("html").css("overflow", "hidden");
+    $(".darkwindow").css("top", window.pageYOffset + "px");
+
+}
+
+function closeOrder() {
+    $(".containerOfP").css("display", "block");
+    $(".orderInput").css("display", "flex");
+    $(".waitCall").css("display", "none");
+    $(".darkwindow").css("display", "none");
+    $("html").css("overflow", "visible");
+    let a = $(".text");
+    a[0].value = "";
+    a = $(".tel");
+    a[0].value = "";
+}
+
+function wait() {
+    $(".containerOfP, .orderInput").slideUp();
+    setTimeout(function () {
+        $(".waitCall").slideToggle();
+    }, 100);
+}
--- scriptFrame.js
+++ scriptFrame.js
@@ -1,7 +1,8 @@
 'use strict'
 
-let i=4;
-function slide(a){
+let i = 4;
+
+function slide(a) {
 
     var el = document.getElementsByTagName("img");
     var op = 1;
@@ -13,64 +14,66 @@ function slide(a){
         el[11].style.opacity = op;
         el[12].style.opacity = op;
         op -= 0.1;
-        setTimeout (func, 30);
-        
-    },60);   
+        setTimeout(func, 30);
+
+    }, 60);
     setTimeout(switchImg, 400);
     setTimeout(opca, 400);
-    function switchImg(){
+
+    function switchImg() {
         console.log("op");
         i++;
-        if(i>8){
-            i=1;
+        if (i > 8) {
+            i = 1;
         }
         console.log(a);
         console.log(i);
-        switch (i){
-            case 1:{
-                $('.slideOne img').attr("src","/floristry/pictures/frame"+a+"_"+6+".jpg");
-                $('.slideTwo img').attr("src","/floristry/pictures/frame"+a+"_"+7+".jpg");
-                $('.slideThree img').attr("src","/floristry/pictures/frame"+a+"_"+8+".jpg");
-                $('.slideFour img').attr("src","/floristry/pictures/frame"+a+"_"+i+".jpg");
+        switch (i) {
+            case 1: {
+                $('.slideOne img').attr("src", "/floristry/pictures/frame" + a + "_" + 6 + ".jpg");
+                $('.slideTwo img').attr("src", "/floristry/pictures/frame" + a + "_" + 7 + ".jpg");
+                $('.slideThree img').attr("src", "/floristry/pictures/frame" + a + "_" + 8 + ".jpg");
+                $('.slideFour img').attr("src", "/floristry/pictures/frame" + a + "_" + i + ".jpg");
                 break;
             }
-            case 2:{
-                $('.slideOne img').attr("src","/floristry/pictures/frame"+a+"_"+7+".jpg");
-                $('.slideTwo img').attr("src","/floristry/pictures/frame"+a+"_"+8+".jpg");
-                $('.slideThree img').attr("src","/floristry/pictures/frame"+a+"_"+(i-1)+".jpg");
-                $('.slideFour img').attr("src","/floristry/pictures/frame"+a+"_"+i+".jpg");
+            case 2: {
+                $('.slideOne img').attr("src", "/floristry/pictures/frame" + a + "_" + 7 + ".jpg");
+                $('.slideTwo img').attr("src", "/floristry/pictures/frame" + a + "_" + 8 + ".jpg");
+                $('.slideThree img').attr("src", "/floristry/pictures/frame" + a + "_" + (i - 1) + ".jpg");
+                $('.slideFour img').attr("src", "/floristry/pictures/frame" + a + "_" + i + ".jpg");
                 break;
             }
-            case 3:{
-                $('.slideOne img').attr("src","/floristry/pictures/frame"+a+"_"+8+".jpg");
-                $('.slideTwo img').attr("src","/floristry/pictures/frame"+a+"_"+(i-2)+".jpg");
-                $('.slideThree img').attr("src","/floristry/pictures/frame"+a+"_"+(i-1)+".jpg");
-                $('.slideFour img').attr("src","/floristry/pictures/frame"+a+"_"+i+".jpg");
+            case 3: {
+                $('.slideOne img').attr("src", "/floristry/pictures/frame" + a + "_" + 8 + ".jpg");
+                $('.slideTwo img').attr("src", "/floristry/pictures/frame" + a + "_" + (i - 2) + ".jpg");
+                $('.slideThree img').attr("src", "/floristry/pictures/frame" + a + "_" + (i - 1) + ".jpg");
+                $('.slideFour img').attr("src", "/floristry/pictures/frame" + a + "_" + i + ".jpg");
                 break;
             }
-            default:{
-                $('.slideOne img').attr("src","/floristry/pictures/frame"+a+"_"+(i-3)+".jpg");
-                $('.slideTwo img').attr("src","/floristry/pictures/frame"+a+"_"+(i-2)+".jpg");
-                $('.slideThree img').attr("src","/floristry/pictures/frame"+a+"_"+(i-1)+".jpg");
-                $('.slideFour img').attr("src","/floristry/pictures/frame"+a+"_"+i+".jpg");
+            default: {
+                $('.slideOne img').attr("src", "/floristry/pictures/frame" + a + "_" + (i - 3) + ".jpg");
+                $('.slideTwo img').attr("src", "/floristry/pictures/frame" + a + "_" + (i - 2) + ".jpg");
+                $('.slideThree img').attr("src", "/floristry/pictures/frame" + a + "_" + (i - 1) + ".jpg");
+                $('.slideFour img').attr("src", "/floristry/pictures/frame" + a + "_" + i + ".jpg");
                 break
             }
         }
     }
-    function opca(){
-    var el = document.getElementsByTagName("img");
-    var op = 0;
-    setTimeout(function func() {
-        if (op > 1)
-            return;
-        el[9].style.opacity = op;
-        el[10].style.opacity = op;
-        el[11].style.opacity = op;
-        el[12].style.opacity = op;
-        op += 0.1;
-        setTimeout (func, 30);
-        
-    });
+
+    function opca() {
+        var el = document.getElementsByTagName("img");
+        var op = 0;
+        setTimeout(function func() {
+            if (op > 1)
+                return;
+            el[9].style.opacity = op;
+            el[10].style.opacity = op;
+            el[11].style.opacity = op;
+            el[12].style.opacity = op;
+            op += 0.1;
+            setTimeout(func, 30);
+
+        });
     }
 }
 $(".facebookIcon").hover(function () {
@@ -95,50 +98,52 @@ $(".vkIcon").hover(function () {
     $(this).attr("src", "/floristry/pictures/vkIcon.svg");
 });
 let check = true;
-$(".burgerMenuDeckstop").click(function(){
+$(".burgerMenuDeckstop").click(function () {
     console.log("LLL");
-    if(check){
+    if (check) {
         $(".left, .right").slideToggle();
-        setTimeout(slideHead,500);
-    }
-    else{
+        setTimeout(slideHead, 500);
+    } else {
         slideHead();
-        setTimeout(function(){
+        setTimeout(function () {
             $(".left, .right").slideToggle();
-        },500);
+        }, 500);
     }
-    function slideHead(){
+
+    function slideHead() {
         $("header").slideToggle();
-        if(check){
-            $(".burgerMenuDeckstop img").attr("src","/floristry/pictures/closeMenuDeck.svg");
-            $(".burgerMenuDeckstop").css("position","static");
-            $(".burgerMenuDeckstop").css("margin-right","5%");
-            check=!check;
-        }
-        else{
-            $(".burgerMenuDeckstop img").attr("src","/floristry/pictures/burgerMenuDeckstop.svg");
-            $(".burgerMenuDeckstop").css("position","relative");
-            check=!check;
+        if (check) {
+            $(".burgerMenuDeckstop img").attr("src", "/floristry/pictures/closeMenuDeck.svg");
+            $(".burgerMenuDeckstop").css("position", "static");
+            $(".burgerMenuDeckstop").css("margin-right", "5%");
+            check = !check;
+        } else {
+            $(".burgerMenuDeckstop img").attr("src", "/floristry/pictures/burgerMenuDeckstop.svg");
+            $(".burgerMenuDeckstop").css("position", "relative");
+            check = !check;
         }
     }
-   
+
 });
- function order(){
-     $(".darkwindow").css("display","block");
- }
- function closeOrder(){
-    $(".containerOfP").css("display","block");
-    $(".orderInput").css("display","flex");
-    $(".waitCall").css("display","none");
-    $(".darkwindow").css("display","none");
+
+function order() {
+    $(".darkwindow").css("display", "block");
+}
+
+function closeOrder() {
+    $(".containerOfP").css("display", "block");
+    $(".orderInput").css("display", "flex");
+    $(".waitCall").css("display", "none");
+    $(".darkwindow").css("display", "none");
     let a = $(".text");
-     a[0].value="";
-     a=$(".tel");
-     a[0].value="";
- }
- function wait(){
-     $(".containerOfP, .orderInput").slideUp();
-     setTimeout(function(){
-         $(".waitCall").slideToggle();
-     },100);
- }
+    a[0].value = "";
+    a = $(".tel");
+    a[0].value = "";
+}
+
+function wait() {
+    $(".containerOfP, .orderInput").slideUp();
+    setTimeout(function () {
+        $(".waitCall").slideToggle();
+    }, 100);
+}
